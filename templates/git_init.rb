f7fb4211ea3443c0816a59f2049eed446df5845e
Anders Törnqvist <anders@elabs.se>
2009-04-17 21:58:02 +0200

    working on git_init.rb

--- templates/git_init.rb
+++ templates/git_init.rb
@@ -1,3 +1,23 @@
+#########################
+#  SCM
+#########################
 git :init
+ 
+file('.gitignore') do
+  '.DS_Store
+  config/database.yml
+  coverage/*
+  db/*.sqlite3
+  db/schema.rb
+  log/*.log
+  tmp/**/*'
+end 
+ 
+# tell git to hold empty directories
+#run %{find . -type d -empty | grep -v ".git" | xargs -I xxx touch xxx/.gitignore}
+run "touch log/.gitignore tmp/.gitignore vendor/.gitignore"
+ 
+run "cp config/database.yml config/database.yml.example"
+ 
 git :add => "."
-git :commit => "-a -m 'Initial commit'"
+git :commit => "-m 'initial commit'"
