cd2bb52b9dcd3150eed35b19e930ff34f59f1e30
Anders Törnqvist <anders@elabs.se>
2009-04-17 22:22:10 +0200

    working on git_init.rb and basic.rb

--- generate.rb
+++ generate.rb
@@ -1,3 +1,4 @@
 TEMPLATE_ROOT = "http://github.com/unders/rails-1-2-3-launcher/raw/master/templates/"
 
 load_template(TEMPLATE_ROOT + "git_init.rb")
+load_template(TEMPLATE_ROOT + "basic.rb")
--- templates/git_init.rb
+++ templates/git_init.rb
@@ -2,21 +2,23 @@
 #  SCM
 #########################
 git :init
- 
-file('.gitignore') do
-  '.DS_Store
+
+# Set up .gitignore files
+file '.gitignore', <<-END
+.DS_Store
 config/database.yml
 coverage/*
 db/*.sqlite3
 db/schema.rb
 log/*.log
-tmp/**/*'
-end 
+tmp/**/*
+END
  
 # tell git to hold empty directories
-#run %{find . -type d -empty | grep -v ".git" | xargs -I xxx touch xxx/.gitignore}
 run "touch log/.gitignore tmp/.gitignore vendor/.gitignore"
- 
+run %{find . -type d -empty | grep -v "vendor" | grep -v ".git" | grep -v "tmp" | xargs -I xxx touch xxx/.gitignore}
+
+# Copy database.yml for distribution use 
 run "cp config/database.yml config/database.yml.example"
  
 git :add => "."
