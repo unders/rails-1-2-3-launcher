84e234bddeff2410dedc94b94dac0ea1d895112c
Anders Törnqvist <anders@elabs.se>
2009-04-17 23:20:10 +0200

    Added bdd_stack.rb

--- generate.rb
+++ generate.rb
@@ -2,3 +2,4 @@ TEMPLATE_ROOT = "http://github.com/unders/rails-1-2-3-launcher/raw/master/templa
 
 load_template(TEMPLATE_ROOT + "git_init.rb")
 load_template(TEMPLATE_ROOT + "basic.rb")
+load_template(TEMPLATE_ROOT + "bdd_stack.rb")
---
+++ templates/bdd_stack.rb
@@ -0,0 +1,34 @@
+gem('rspec', :lib => false, :version => ">= 1.2.4")
+gem('rspec-rails', :lib => false, :version => ">= 1.2.4")
+
+# in config/environments/test.rb
+# config.gem "rspec", :lib => false, :version => ">= 1.2.4" 
+# config.gem "rspec-rails", :lib => false, :version => ">= 1.2.4"
+# gem
+# gem rspec-rails-1.2.4
+# gem rspec-1.2.4
+
+# generate("rspec")
+
+
+#gem install term-ansicolor treetop diff-lcs nokogiri builder
+
+# gem cucumber-0.3.0
+# gem webrat-0.4.4 - http://gitrdoc.com/brynary/webrat/tree/master/
+
+# generate("cucumber")
+
+# config.gem "remarkable_rails", :lib => false, :version => ">= 3.0.3"
+# gem remarkable_rails-3.0.3 http://github.com/carlosbrando/remarkable/tree/master
+
+# And then require remarkable inside your spec_helper.rb, after "spec/rails":
+# require 'spec/rails'
+# require 'remarkable_rails'
+
+# sudo gem install notahat-machinist --source http://gems.github.com
+
+# faker-0.3.1 - http://faker.rubyforge.org/rdoc/
+# gem install populator-0.2.5 - http://github.com/ryanb/populator/tree/master
+
+
+
