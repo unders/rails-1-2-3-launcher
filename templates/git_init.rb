ec42587966dd3e59e041d7d01c0cbff175a70daa
Anders Törnqvist <anders@elabs.se>
2009-04-18 13:38:09 +0200

    removed mysql from database.yml

--- templates/git_init.rb
+++ templates/git_init.rb
@@ -19,39 +19,7 @@ EOS
 run "touch log/.gitignore tmp/.gitignore vendor/.gitignore"
 run %{find . -type d -empty | grep -v "vendor" | grep -v ".git" | grep -v "tmp" | xargs -I xxx touch xxx/.gitignore}
 
-app_db = File.basename(root).gsub(/[-\s]/, '_').downcase
-file 'config/database.yml', <<-EOS.gsub(/^  /, '')
-development:
-  adapter: mysql
-  encoding: utf8
-  reconnect: false
-  database: #{app_db}_development
-  pool: 5
-  username: root
-  password:
-  socket: /tmp/mysql.sock
 
-test:
-  adapter: mysql
-  encoding: utf8
-  reconnect: false
-  database: #{app_db}_test
-  pool: 5
-  username: root
-  password:
-  socket: /tmp/mysql.sock
-
-production:
-  adapter: mysql
-  encoding: utf8
-  reconnect: false
-  database: #{app_db}_production
-  pool: 5
-  username: root
-  password: 
-EOS
-# Copy database.yml for distribution use
-  run "cp config/database.yml config/database.yml.example"
 # Copy database.yml for distribution use 
 run "cp config/database.yml config/database.yml.example"
  
