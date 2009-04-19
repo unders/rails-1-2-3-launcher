0786e1ea22bd2d93eb6ec8444fbf7db9421b07ac
Anders Törnqvist <anders@elabs.se>
2009-04-19 22:13:18 +0200

    ...

--- templates/bdd_stack.rb
+++ templates/bdd_stack.rb
@@ -189,7 +189,7 @@ namespace :tarantula do
   task :report => :test do
     Dir.glob("tmp/tarantula/**/index.html") do |file|
       if PLATFORM['darwin']
-        system("open #{file}")
+        #system("open #{file}")
 TASK
 end            
 # http://github.com/tapajos/integration/tree/master
