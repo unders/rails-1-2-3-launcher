9ca801d4564eecf0842ae3a040141f0e23b473f5
Anders Törnqvist <anders@elabs.se>
2009-04-28 21:38:26 +0200

    plugin 'safe_mass_assignment'; gem 'chardet'; gem 'html5'; plugin 'xss_terminate'

--- templates/app_stack.rb
+++ templates/app_stack.rb
@@ -1,4 +1,30 @@
+initializer("inaccessible_attributes.rb.rb") do
+<<-CODE
+ActiveRecord::Base.send(:attr_accessible, nil) 
+
+if Rails.env.test? or Rails.env.development?
+
+  module ActiveRecord
+    class MassAssignmentError < StandardError; end
+  end
+
+  ActiveRecord::Base.class_eval do
+    def log_protected_attribute_removal(*attributes)
+      raise ActiveRecord::MassAssignmentError, "Can't mass-assign these protected attributes: #{attributes.join(', ')}"
+    end
+  end
+end
+CODE
+end
+
+plugin 'safe_mass_assignment', :git => 'http://github.com/jamis/safe_mass_assignment/tree/master'
+
+gem 'chardet', :version => ">= 0.9.0"
+gem 'html5', :version => ">= 0.10.0"
+plugin 'xss_terminate', :git => 'git://github.com/look/xss_terminate.git'
+
 gem 'validate_options', :version => ">= 0.0.2"
+gem 'active_presenter', :version => ">= 1.1.2"
 
 rake "gems:install", :sudo => true
 rake "gems:unpack"
---
+++ templates/javascript_stack.rb
@@ -0,0 +1,4 @@
+# http://ajaxian.com/archives/announcing-ajax-libraries-api-speed-up-your-ajax-apps-with-googles-infrastructure
+# http://googleajaxsearchapi.blogspot.com/2008/05/speed-up-access-to-your-favorite.html
+# http://code.google.com/apis/ajaxlibs/
+# http://code.google.com/apis/ajaxlibs/documentation/index.html
