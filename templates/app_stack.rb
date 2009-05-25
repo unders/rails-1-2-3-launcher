1f86e65089bb14dde9cf82fa61f07d2934ddeeeb
Anders Törnqvist <anders@elabs.se>
2009-05-25 15:08:12 +0200

    ...

--- templates/app_stack.rb
+++ templates/app_stack.rb
@@ -17,10 +17,10 @@ end
 CODE
 end
 
-plugin 'safe_mass_assignment', :git => 'http://github.com/jamis/safe_mass_assignment/tree/master'
+#plugin 'safe_mass_assignment', :git => 'http://github.com/jamis/safe_mass_assignment/tree/master'
 
-gem 'chardet', :version => ">= 0.9.0", :lib => false
-gem 'html5', :version => ">= 0.10.0"
+#gem 'chardet', :version => ">= 0.9.0", :lib => false
+#gem 'html5', :version => ">= 0.10.0"
 plugin 'xss_terminate', :git => 'git://github.com/look/xss_terminate.git'
 inside('vendor/plugins/xss_terminate/test') do
   run("rm xss_terminate_test.rb")
@@ -30,5 +30,5 @@ end
 #gem 'validate_options', :version => ">= 0.0.2"
 #gem 'active_presenter', :version => ">= 1.1.2"
 
-rake "gems:install", :sudo => true
-rake "gems:unpack"
+#rake "gems:install", :sudo => true
+#rake "gems:unpack"
