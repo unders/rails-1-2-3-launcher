d3899adcd71d5dfd1b95a19990e3629685d93e72
Jonas Nicklas and Anders Tornqvist <dev+jnicklas+unders@elabs.se>
2009-06-03 10:31:59 +0200

    added auth

--- templates/auth.rb
+++ templates/auth.rb
@@ -1,2 +1,17 @@
-http://pastie.org/439436
-http://github.com/stffn/declarative_authorization/tree/master
+gem "thoughtbot-clearance", :lib => 'clearance', :source  => 'http://gems.github.com',  :version => '>=0.6.4'
+rake "gems:install", :sudo => true
+
+generate "clearance"
+rake "db:migrate"
+
+hostname = ask?("what is the hostname in development?")
+initializer('clearance.rb', <<-FILE)
+if Rails.env.development?
+  HOST = "#{hostname}"
+elsif Rails.env.test?
+  HOST = "test.host"
+else
+  HOST = "foo.bar"
+end
+DO_NOT_REPLY = "support@elabs.se"
+FILE
