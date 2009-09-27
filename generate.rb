39ea3bc9ae1c4de89ca681e965dc2c3be4bc311e
Anders Törnqvist <anders@elabs.se>
2009-09-27 20:28:12 +0200

    updagte to generate

--- generate.rb
+++ generate.rb
@@ -1,4 +1,5 @@
 TEMPLATE_ROOT = "http://github.com/unders/rails-1-2-3-launcher/raw/master/templates/"
+#TEMPLATE_ROOT = "/Users/unders/homerails/rails-1-2-3-launcher/templates/"
 
 load_template(TEMPLATE_ROOT + "helper.rb")
 load_template(TEMPLATE_ROOT + "git_init.rb")
