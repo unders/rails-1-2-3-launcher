02d3045b40cdd54a8c6b0ba8f18a665c154810e1
Jonas Nicklas and Anders Tornqvist <dev+jnicklas+unders@elabs.se>
2009-09-27 16:16:41 +0200

    updates to bdd stack

--- templates/bdd_stack.rb
+++ templates/bdd_stack.rb
@@ -1,27 +1,37 @@
 # All gems are added to config/environments/test.rb and installed to the system if not already installed.
 
-gem 'ZenTest', :version => ">=4.0.0", :env => 'test'
+gem 'ZenTest', :version => ">=4.1.4", :env => 'test'
 
-gem 'rspec', :lib => false, :version => ">= 1.2.4", :env => 'test'
-gem 'rspec-rails', :lib => false, :version => ">= 1.2.4", :env => 'test'
+gem 'rspec', :lib => false, :version => ">= 1.2.8", :env => 'test'
+gem 'rspec-rails', :lib => false, :version => ">= 1.2.7.1", :env => 'test'
+
 
 rake "gems:install", :env => "test", :sudo => true
 generate "rspec"
+
+git :add => "."
+git :commit => "-m 'added rspec dependency to test environment'"
 
 # Cucumber gem dependencies
 %w{term-ansicolor treetop diff-lcs nokogiri builder}.each do |package|
   gem package, :lib => false, :env => 'test'
 end
 
-gem 'cucumber', :version => ">= 0.3.0", :env => 'test'
-gem 'webrat', :version => ">= 0.4.4", :env => 'test'
+gem 'cucumber', :version => ">= 0.3.103", :env => 'test'
+gem 'webrat', :version => ">= 0.5.0", :env => 'test'
 rake "gems:install", :env => "test", :sudo => true
 generate "cucumber" 
 
-gem 'remarkable_rails', :lib => false, :version => ">= 3.0.3", :env => 'test'
+git :add => "."
+git :commit => "-m 'added cucumber and webrat dependencies to test environment'"
+
+gem 'remarkable_rails', :lib => false, :version => ">= 3.1.10", :env => 'test'
 file_inject 'spec/spec_helper.rb', "require 'spec/rails'", "require 'remarkable_rails'"
 
-gem 'bmabey-email_spec', :version => '>= 0.1.3', 
+git :add => "."
+git :commit => "-m 'added remarkable-rails to test environment'"
+
+gem 'bmabey-email_spec', :version => '>= 0.3.4', 
                          :lib => 'email_spec', 
                          :source => 'http://gems.github.com',
                          :env => 'test'
@@ -36,66 +46,105 @@ file_inject 'spec/spec_helper.rb',
 rake "gems:install", :env => "test", :sudo => true                         
 generate :email_spec
 
-gem 'faker', :version => '>=0.3.1', :env => 'test'
-# gem 'sevenwire-forgery', :version => '>= 0.2.2', 
-#                          :lib => 'forgery', 
-#                          :source => 'http://gems.github.com', 
-#                          :env => 'test'
+git :add => "."
+git :commit => "-m 'added email_spec dependency to test environment'"
 
+
+gem 'faker', :version => '>=0.3.1', :env => 'test'
 rake "gems:install", :env => "test", :sudo => true
-#generate :forgery
+
+git :add => "."
+git :commit => "-m 'added faker dependency to test environment'"
+
 
 gem 'notahat-machinist', :lib => 'machinist', :source => "http://gems.github.com", :env => 'test'
+rake "gems:install", :env => "test", :sudo => true
 file_inject 'spec/spec_helper.rb', 
             "require 'email_spec/matchers'", 
             "require File.expand_path(File.dirname(__FILE__) + '/blueprints')"
 file_inject 'spec/spec_helper.rb',
             'config.include(EmailSpec::Matchers)', 
-            '  config.before(:each) { Sham.reset }'     
+            '  config.before(:each) { Sham.reset(:before_each) }'
 file_inject 'features/support/env.rb',
             "require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')",
             "require File.join(RAILS_ROOT, 'spec', 'blueprints')"
+file_append('config/environments/cucumber.rb', "config.gem 'bmabey-email_spec', :lib => 'email_spec', :version => '>= 0.3.4'")             
             
 file 'spec/blueprints.rb' do
 <<-CODE
+# encoding: utf-8
 require 'machinist/active_record'
+require 'sham'
 require 'faker'
 
+# In Textmate use: http://github.com/drnic/ruby-machinist-tmbundle/
+# Consider a BlogPost that has many BlogComments in a blog app. If you go to your blueprints.rb file, type "BlogPost" 
+# and press "Cmd+B" the bundle will load up the BlogPost class, inspect it, and generate the following a blueprint.
+
 # Shams
-# We use forgery (and faker) to make up some test data
+# We use faker to make up some test data
 
 Sham.define do
-  name { Faker::Name.name } 
-  email { Faker::Internet.email }
-  login { Faker::Internet.user_name }
-  title { Faker::Lorem.sentence }
-  text { Faker::Lorem.paragraphs }
-  time { DateTime.civil(1990 + rand(20), rand(12)+1, rand(28)+1, rand(24), rand(60), rand(60)) }
-  date { Date.civil(1990 + rand(20), rand(12)+1, rand(28)+1) }
+  name                               { Faker::Name.name } 
+  first_name                         { Faker::Name.first_name } 
+  last_name                          { Faker::Name.last_name }
+  login                              { Faker::Internet.user_name.gsub('.', '') }
+  username                           { Faker::Internet.user_name.gsub('.', '') }
+  email                              { Faker::Internet.email }
+  company_name                       { Faker::Company.name } 
+  title                              { Faker::Lorem.sentence }
+  text                               { Faker::Lorem.paragraphs.join('\\n\\n') }
+  body                               { Faker::Lorem.paragraphs.join('\\n\\n') }
+#  picture                            { File.open Dir.glob(File.join(File.dirname(__FILE__), 'fixtures/*.jpg')).to_a.rand }
+  time                               { DateTime.civil(1990 + rand(20), rand(12)+1, rand(28)+1, rand(24), rand(60), rand(60)) }
+  date                               { Date.civil(1990 + rand(20), rand(12)+1, rand(28)+1) }
+  percentage(:unique => false)       { rand(100) }
+  price(:unique => false)            { BigDecimal.new("#{rand(1000)}.00") }
+  corporate_identity_number          { ("%010d" % rand(10000000000)) }
+  quantity(:unique => false)         { rand(10) + 1 }
+  pwd(:unique => false)              { 'test1234' }
 end
  
 # Blueprints 
-User.blueprint do
-  pwd = "test1234"
-  email { Sham.email }
-  password { pwd }
-  password_confirmation { pwd }
-end
+# User.blueprint do
+#   email                 { Sham.email }
+#   password              { Sham.pwd }
+#   password_confirmation { Sham.pwd }
+# end
+
+# Example
+# User.blueprint do
+#   name
+#   email
+# end
+# 
+# User.blueprint(:admin) do
+#   name  { Sham.name + " (admin)" }
+#   admin { true }
+# end
+# 
+# Calling:
+# User.make(:admin)
+# Named blueprints call the default blueprint to set any attributes not specifically provided, so in this example 
+# the email attribute will still be generated even for an admin user.
 
 CODE
 end            
-                            
+
+git :add => "."
+git :commit => "-m 'added machinist dependency to test environment'"
 
 plugin 'time_travel', :git => 'git://github.com/notahat/time_travel.git'
+file_inject('vendor/plugins/time_travel/spec/time_travel_spec.rb', "require 'time'", "require 'rubygems'")
 
-#gem :populator, :version => '>=0.2.5', :env => 'test'
+git :add => "."
+git :commit => "-m 'added time_travel dependency to test environment'"
 
 file 'lib/tasks/populate.rake' do
 <<-CODE
+# encoding: utf-8
+
 # lib/tasks/populate.rake
-# http://github.com/ryanb/populator/tree/master
-# http://railscasts.com/episodes/126-populating-a-database
-# http://populator.rubyforge.org/
 
 module Populate
   extend self
@@ -129,91 +178,86 @@ end
 CODE
 end
 
-rake "gems:install", :env => "test", :sudo => true
+git :add => "."
+git :commit => "-m 'added lib/tasks/populate.rake'"
 
-plugin 'be_valid_asset', :git => 'git://github.com/unboxed/be_valid_asset.git'
+gem 'unboxed-be_valid_asset', :version => '>= 1.1.0', 
+                              :lib => 'be_valid_asset', 
+                              :source => 'http://gems.github.com',
+                              :env => 'test'
+                         
+rake "gems:install", :env => "test", :sudo => true
 file_inject 'spec/spec_helper.rb',
-            'config.before(:each) { Sham.reset }', 
+            'config.before(:each) { Sham.reset(:before_each) }',
             '  config.include BeValidAsset'
 
 file_inject 'spec/spec_helper.rb',
             'config.include BeValidAsset',            
-            %q{BeValidAsset::Configuration.enable_caching = true
-BeValidAsset::Configuration.cache_path = File.join(RAILS_ROOT, %w(tmp be_valid_asset_cache))}
-
-
-
-# rack-bug has depency on rack-test and sintra gems; command: rake spec:plugins will fail
-# if they aren't installed
-#gem 'rack-test', :version => '>= 0.2.0', :env => "test"
-#gem 'sinatra', :version => '>= 0.9.1.1', :env => "test"
-rake "gems:install", :env => "test", :sudo => true
-
-# sudo gem install fakeweb fakeweb-1.2.0
-
-#Rspec has a similar function built in. You can get benchmark info on your 10 slowest test by adding:
-#--format profile
-#to spec/spec.opts (or passing it as a command line option to the spec script).
-#http://bitfission.com/blog/2009/02/profiling-rspec.html
-
-
-
-# http://github.com/jeremy/ruby-prof/tree/master
-# gem install ruby-prof
-# http://snippets.aktagon.com/snippets/255-How-to-profile-your-Rails-and-Ruby-applications-with-ruby-prof
-
-
-
-#http://github.com/grosser/single_test/tree/master
-
-
-
-# http://drnicwilliams.com/2008/01/04/autotesting-javascript-in-rails/
-# http://github.com/drnic/jsunittest/tree/master
-
-# or
-
-# http://github.com/relevance/blue-ridge/tree/master
-
-# * sudo gem install autotest_screen
-
-# http://github.com/dchelimsky/rspec-rails/blob/master/generators/rspec/templates/script/spec_server
-# http://wiki.github.com/dchelimsky/rspec/spec_server-autospec-nearly-pure-bdd-joy
-
+            %q{  BeValidAsset::Configuration.enable_caching = true
+  BeValidAsset::Configuration.cache_path = File.join(RAILS_ROOT, %w(tm p be_valid_asset_cache))
+  BeValidAsset::Configuration.display_invalid_content = false (default)
+  BeValidAsset::Configuration.markup_validator_host = 'validator.w3.org'
+  BeValidAsset::Configuration.markup_validator_path = '/check'
+  BeValidAsset::Configuration.css_validator_host = 'jigsaw.w3.org'
+  BeValidAsset::Configuration.css_validator_path = '/css-validator/validator'
+  BeValidAsset::Configuration.feed_validator_host = 'validator.w3.org'
+  BeValidAsset::Configuration.feed_validator_path = '/feed/check.cgi'}
+  
 git :add => "."
-git :commit => "-m 'added bdd_stack'"
+git :commit => "-m 'added gem dependency be_valid_asset (end of bdd_stack)'"  
 
 
-# Needs more investigation:
 
-#1.
-#gem 'ianwhite-pickle', :version => '>= 0.1.12', 
-#                       :lib => 'pickle', 
-#                       :source => 'http://gems.github.com', 
-#                       :env => 'test'
-#rake "gems:install", :env => "test", :sudo => true
-#
-#generate :pickle, "paths email"
+ 
+# # sudo gem install fakeweb fakeweb-1.2.0
+# 
+# #Rspec has a similar function built in. You can get benchmark info on your 10 slowest test by adding:
+# #--format profile
+# #to spec/spec.opts (or passing it as a command line option to the spec script).
+# #http://bitfission.com/blog/2009/02/profiling-rspec.html
+# 
+# 
+# 
+# # http://github.com/jeremy/ruby-prof/tree/master
+# # gem install ruby-prof
+# # http://snippets.aktagon.com/snippets/255-How-to-profile-your-Rails-and-Ruby-applications-with-ruby-prof
 
-#2.
-# Mocka in cucumber
-# http://gist.github.com/80554
-# http://www.brynary.com/2009/2/3/cucumber-step-definition-tip-stubbing-time
-
-#3.
-
-# http://www.somethingnimble.com/bliki/deep-test-1_2_0
-# http://www.somethingnimble.com/bliki/deep-test
-# http://deep-test.rubyforge.org/
-# http://wiki.github.com/aslakhellesoy/cucumber/using-rcov-with-cucumber-and-rails
-# http://github.com/langalex/culerity/tree/master - Integrates Cucumber and Celerity to test Javascript in webapps.
-# http://github.com/brynary/testjour/tree/master
-
-# references:
-# http://www.brynary.com/2009/4/22/rack-bug-debugging-toolbar-in-four-minutes
-# http://github.com/brynary/rack-bug/tree/master
-# webrat-0.4.4 - http://gitrdoc.com/brynary/webrat/tree/master/
-# http://wiki.github.com/bmabey/email-spec/use-cucumber-to-test-email
-# http://wiki.github.com/bmabey/email-spec/use-cucumber-to-test-mailers-in-rails
-# http://www.benmabey.com/2009/02/05/leveraging-test-data-builders-in-cucumber-steps/
-# http://themomorohoax.com/2009/03/08/rails-machinist-tutorial-machinist-with-cucumber-in-10-minutes
+ 
+ 
+# # http://github.com/dchelimsky/rspec-rails/blob/master/generators/rspec/templates/script/spec_server
+# # http://wiki.github.com/dchelimsky/rspec/spec_server-autospec-nearly-pure-bdd-joy
+# 
+# 
+# 
+# # Needs more investigation:
+# 
+# #1.
+# #gem 'ianwhite-pickle', :version => '>= 0.1.12', 
+# #                       :lib => 'pickle', 
+# #                       :source => 'http://gems.github.com', 
+# #                       :env => 'test'
+# #rake "gems:install", :env => "test", :sudo => true
+# #
+# #generate :pickle, "paths email"
+# 
+# #2.
+# # Mocka in cucumber
+# # http://gist.github.com/80554
+# # http://www.brynary.com/2009/2/3/cucumber-step-definition-tip-stubbing-time
+# 
+# #3.
+# 
+# # http://www.somethingnimble.com/bliki/deep-test-1_2_0
+# # http://www.somethingnimble.com/bliki/deep-test
+# # http://deep-test.rubyforge.org/
+# # http://wiki.github.com/aslakhellesoy/cucumber/using-rcov-with-cucumber-and-rails
+# # http://github.com/langalex/culerity/tree/master - Integrates Cucumber and Celerity to test Javascript in webapps.
+# # http://github.com/brynary/testjour/tree/master
+# 
+# # references:
+# # http://www.brynary.com/2009/4/22/rack-bug-debugging-toolbar-in-four-minutes
+# # http://github.com/brynary/rack-bug/tree/master
+# # http://wiki.github.com/bmabey/email-spec/use-cucumber-to-test-email
+# # http://wiki.github.com/bmabey/email-spec/use-cucumber-to-test-mailers-in-rails
+# # http://www.benmabey.com/2009/02/05/leveraging-test-data-builders-in-cucumber-steps/
+# # http://themomorohoax.com/2009/03/08/rails-machinist-tutorial-machinist-with-cucumber-in-10-minutes
