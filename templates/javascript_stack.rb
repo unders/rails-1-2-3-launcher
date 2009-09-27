eae798cd60d364ea5b7a7ec13eab0366eb92d762
Anders Törnqvist <anders@elabs.se>
2009-09-27 20:27:30 +0200

    updated javascript stack

--- templates/javascript_stack.rb
+++ templates/javascript_stack.rb
@@ -2,8 +2,8 @@
 # http://googleajaxsearchapi.blogspot.com/2008/05/speed-up-access-to-your-favorite.html
 # http://code.google.com/apis/ajaxlibs/
 # http://code.google.com/apis/ajaxlibs/documentation/index.html
-#http://github.com/christianhellsten/jquery-google-analytics/tree/master
-#http://github.com/danwrong/low-pro-for-jquery/tree/master
+# http://github.com/christianhellsten/jquery-google-analytics/tree/master
+# http://github.com/danwrong/low-pro-for-jquery/tree/master
 
 # Testing
 # http://drnicwilliams.com/2008/01/04/autotesting-javascript-in-rails/
@@ -13,10 +13,12 @@
 
 # http://github.com/relevance/blue-ridge/tree/master
 
+
 run "curl -s -L http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > public/javascripts/jquery.js"
 run "touch public/javascripts/application.js"
-
+git :add => ".", :commit => "-m 'Added jquery'"
+ 
 plugin "blue-ridge", :git => 'git://github.com/unders/blue-ridge.git'
 generate :blue_ridge
 
-git :add => ".", :commit => "-m 'Added jquery and blue-ridge'"
+git :add => ".", :commit => "-m 'Added blue-ridge'"
